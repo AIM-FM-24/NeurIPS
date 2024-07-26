62a5b6c7e08847fe14d6cdbb1a2df1219aec6f78
AIM-FM-24 <2324660158@qq.com>
2024-07-26 09:31:41 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -347,6 +347,21 @@ The panel discussion members include the following keynote speakers and <a href=
 
 			<div class="grid gap-4">
 				<div class="py-4 max-w-sm rounded overflow-hidden shadow-lg">
+					<a href="https://cobweb.cs.uga.edu/~tliu/" target="_blank" rel="noopener noreferrer">
+						<img class="aspect-square object-cover object-top" src="TianmingLiu.png" style="width: 93%; margin: auto;" alt="Prof. Tianming Liu">
+					</a>
+					<div class="px-6 py-4">
+					<!-- <div class="font-bold text-xl mb-2"><a href="https://cobweb.cs.uga.edu/~tliu/">Prof. Tianming Liu</a></div> -->
+					<div class="font-bold text-xl mb-2"><a href="https://cobweb.cs.uga.edu/~tliu/" target="_blank" rel="noopener noreferrer">Prof. Tianming Liu</a></div>
+					<p class="text-gray-700 text-base">
+						Distinguished Research Professor at University of Georgia (UGA), USA; Director of UGA Bioimaging Research Center, UGA Institute of Bioinformatics, and UGA Institute of Artificial Intelligence.
+					</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="grid gap-4">
+				<div class="py-4 max-w-sm rounded overflow-hidden shadow-lg">
 					<a href="https://www.ucl.ac.uk/ioo/people/pearse-keane" target="_blank" rel="noopener noreferrer">
 						<img class="aspect-square object-cover object-top" src="PearseKeane.png" style="width: 66%; margin: auto;" alt="Prof. Pearse Keane">
 					</a>
